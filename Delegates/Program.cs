ecc31f08df204d520d5a9d492b3cef6aa1dc399e
rvadityakumar <47914349+rvadityakumar@users.noreply.github.com>
2020-02-10 18:52:21 +0530

    Changes stage1
    Stage one chnages

---
+++ Delegates/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var worker = new Worker();
+            worker.WorkPerformed += new EventHandler<WorkPerformedEventArgs>(Worker_WorkPerformed);
+            worker.WorkCompleted += Worker_WorkCompleted;
+            worker.DoWork(7, WorkType.Cool);
+            Console.Read();
+        }
+
+        private static void Worker_WorkCompleted(object sender, EventArgs e)
+        {
+            Console.WriteLine("Work completed");
+        }
+
+        private static void Worker_WorkPerformed(object sender, WorkPerformedEventArgs e)
+        {
+            Console.WriteLine("Hours worked : "+  e.Hours + " " + e.workType.ToString());
+        }
+    }
+
+    public enum WorkType
+    {
+        GoToMeetings,
+        Golf,
+        Cool
+    }
+}
