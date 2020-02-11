11d47524a323be4a70c45083028f5d787e498439
rvadityakumar <47914349+rvadityakumar@users.noreply.github.com>
2020-02-11 13:38:39 +0530

    After Func Implementatin

---
+++ Delegates/ProcessData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    public class ProcessData
+    {
+        public void Process(int x,int y, BizRulesDelegate del)
+        {
+            var res = del(x, y);
+            Console.WriteLine(res);
+        }
+
+
+        public void ProcessAction(int x,int y,Action<int,int> action)
+        {
+            action(x, y);
+            Console.WriteLine("Action has been performed");
+        }
+
+        public void ProcessFunc(int x,int y,Func<int,int,int> func)
+        {
+            var res = func(x, y);
+            Console.WriteLine(res);
+        }
+    }
+}
--- Delegates/Program.cs
+++ Delegates/Program.cs
@@ -6,13 +6,43 @@ using System.Threading.Tasks;
 
 namespace Delegates
 {
+    public delegate int BizRulesDelegate(int x, int y);
     class Program
     {
+
         static void Main(string[] args)
         {
             var worker = new Worker();
-            worker.WorkPerformed += new EventHandler<WorkPerformedEventArgs>(Worker_WorkPerformed);
-            worker.WorkCompleted += Worker_WorkCompleted;
+            // worker.WorkPerformed += new EventHandler<WorkPerformedEventArgs>(Worker_WorkPerformed);
+            // worker.WorkPerformed += Worker_WorkPerformed;
+            ///* Anonymous method call*/   worker.WorkPerformed += delegate (object sender, WorkPerformedEventArgs e)
+            //   {
+            //       Console.WriteLine("Hours worked :" + e.Hours + " " + e.workType.ToString());
+            //   };
+            /*Lambda Expression*/
+            worker.WorkPerformed += (s, e) =>
+            {
+                Console.WriteLine("Hours worked :" + e.Hours + " " + e.workType.ToString());
+            };
+            worker.WorkCompleted += (s, e) => Console.WriteLine("Work completed");
+
+            /* Custom delegate*/
+            BizRulesDelegate addDel = (x, y) => x + y;
+            BizRulesDelegate mulDel = (x, y) => x * y;
+
+            var data = new ProcessData();
+            data.Process(5, 6, addDel);
+
+
+            /*Action*/
+            Action<int, int> myaction = (x, y) => Console.WriteLine(x/y);
+            data.ProcessAction(56, 6, myaction);
+
+            /* Func */
+            Func<int, int, int> myAddFunc = (x, y) => x + y;
+            data.Process(5, 7, myAddFunc);
+
+            // worker.WorkCompleted += Worker_WorkCompleted;
             worker.DoWork(7, WorkType.Cool);
             Console.Read();
         }
@@ -24,7 +54,7 @@ namespace Delegates
 
         private static void Worker_WorkPerformed(object sender, WorkPerformedEventArgs e)
         {
-            Console.WriteLine("Hours worked : "+  e.Hours + " " + e.workType.ToString());
+            Console.WriteLine("Hours worked : " + e.Hours + " " + e.workType.ToString());
         }
     }
 
