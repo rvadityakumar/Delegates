9741f2f318f70b08b3fb37e350898786d305b9d4
rvadityakumar <47914349+rvadityakumar@users.noreply.github.com>
2020-02-11 15:15:15 +0530

    Changes After Lambda Push

--- Delegates/Program.cs
+++ Delegates/Program.cs
@@ -12,6 +12,24 @@ namespace Delegates
 
         static void Main(string[] args)
         {
+
+            var custs = new List<Customer>
+            {
+            new Customer{FirstName= "Simhachalam",LastName="Chintalapudi",Id=1,City="Tanuku"},
+            new Customer{FirstName= "Annavaram",LastName="Meruduri",Id=2,City="Atthili"},
+            new Customer{FirstName= "Tirupati",LastName="Tottempudi",Id=3,City="Ramvaram"},
+            new Customer{FirstName= "Bhadrachalam",LastName="Relangi",Id=4,City="Tanuku"}
+            };
+            var Tancusts = custs.Where(c => c.City == "Tanuku");
+
+            foreach (var item in Tancusts)
+            {
+                Console.WriteLine(item.FirstName +"-"+ item.City);
+            }
+
+
+
+
             var worker = new Worker();
             // worker.WorkPerformed += new EventHandler<WorkPerformedEventArgs>(Worker_WorkPerformed);
             // worker.WorkPerformed += Worker_WorkPerformed;
@@ -31,16 +49,16 @@ namespace Delegates
             BizRulesDelegate mulDel = (x, y) => x * y;
 
             var data = new ProcessData();
-            data.Process(5, 6, addDel);
+            data.Process(5, 6, addDel)
 
 
             /*Action*/
-            Action<int, int> myaction = (x, y) => Console.WriteLine(x/y);
+            Action<int, int> myaction = (x, y) => Console.WriteLine(x / y);
             data.ProcessAction(56, 6, myaction);
 
             /* Func */
             Func<int, int, int> myAddFunc = (x, y) => x + y;
-            data.Process(5, 7, myAddFunc);
+            data.ProcessFunc(5, 7, myAddFunc);
 
             // worker.WorkCompleted += Worker_WorkCompleted;
             worker.DoWork(7, WorkType.Cool);
